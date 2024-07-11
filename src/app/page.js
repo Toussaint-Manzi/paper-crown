8066333e92112e197e019227134e63744df44e33
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2024-07-11 16:59:57 +0200

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/app/components/Navbar";
 import Image from "next/image";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="outfit">
       <Navbar/>
+      <Footer/>
     </main>
   );
 }
