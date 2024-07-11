1c6caf7fce500bcfa371908a687d99790842c0b4
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2024-07-11 21:01:56 +0200

    added recommended section

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Footer from "./components/Footer";
 import HomeSection from "./components/sections/Home";
 import Join from "./components/sections/Join";
+import Recommend from "./components/sections/Recommend";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar/>
       <HomeSection/>
       <Join/>
+      <Recommend/>
       <Footer/>
     </main>
   );
