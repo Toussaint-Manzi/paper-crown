1046e52f054baeac5f7812877552a2e1138ac5b1
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2024-07-11 16:16:25 +0200

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        '2xl': {'max': '1435px'},
+        'xl': {'max': '1279px'},
+        'xsm': {'min': '480px'},    
+        'msm': {'min': '700px'},
+        'lg': {'max': '1025px'},
+        'larg': {'max': '950px'},
+        'md': {'max': '867px'},
+        'sm': {'max': '700px'},
+        'mdd': {'min': '867px'},
+        'lgm': {'min': '1025px'},
+        'xs': {'max': '500px'},
+        'xtra': {'max': '400px'},    
+      },
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
