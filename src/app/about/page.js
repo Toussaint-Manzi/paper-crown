452f86d31b65e03e376f371690a18b34544f115a
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2024-07-15 12:30:10 +0200

    added story section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,6 +8,7 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 import styles from '@/app/styles/home.module.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import decor from '../../../public/assets/images/decor.png';
 
 export default function About() {
     return (
@@ -66,6 +67,65 @@ export default function About() {
                 </svg>
                 <h1 className={`my-5 text-[#D71A30] text-[22px] font-[600] ${styles.myFont2}`}>Our mission</h1>
                 <h1 className='text-[18px] text-black font-[300] xs:text-[18px]'>We’ll achieve this vision by equipping adolescents, especially girls, to be vocal community activists through participatory, transformative workshops and clubs, so they are empowered to challenge gender norms and break the intergenerational cycle of discrimination and violence.</h1>
+            </div>
+        </div>
+        <div className='flex justify-between items-center w-[77%] mx-auto h-[250px] rounded-[40px] px-[13%] bg-black my-24'>
+            <div className='relative w-[110px] h-[130px]'>
+                <Image
+                    src={decor}
+                    layout="fill"
+                    objectFit="contain"
+                    alt="Person"
+                />
+            </div>
+            <div className='w-[70%] mx-auto'>
+                <h1 className={`text-white text-center font-semibold text-[40px] w-[100%] mx-auto my-4 ${styles.myFont2}`}>Change a heart, change a mind, change the world.</h1>
+            </div>
+            <div className='relative w-[110px] h-[130px]'>
+                <Image
+                    src={decor}
+                    layout="fill"
+                    objectFit="contain"
+                    alt="Person"
+                />
+            </div>
+        </div>
+        <div className='h-full pb-[200px] w-full px-[13%] pt-[200px] flex justify-between items-center 2xl:h-full xl:py-[150px]'>
+            <div className='relative h-[500px] w-[45%] xl:hidden'>
+                <div className='absolute h-[350px] w-[550px] rounded-xl'>
+                <div className="w-full h-full p-1 md:p-2 relative">
+                    <Image
+                        alt="gallery"
+                        className="block h-full w-full rounded-2xl object-cover object-center cursor-pointer"
+                        src="/assets/images/people.png"
+                        fill
+                    />
+                </div>
+                </div>
+                <div className='absolute bottom-[10%] right-[0%] h-[200px] w-[350px] rounded-xl'>
+                <div className="w-full h-full p-1 md:p-2 relative">
+                    <Image
+                        alt="gallery"
+                        className="block h-full w-full rounded-2xl object-cover object-center cursor-pointer"
+                        src="/assets/images/people.png"
+                        fill
+                    />
+                </div>
+                </div>
+            </div>
+            <div className='w-[45%] self-start xl:w-full'>
+                <div className='flex items-center w-[45%] justify-between'>
+                    <div className="h-[2px] w-[80px] bg-black"></div>
+                    <h1 className={`text-[#D71A30] text-xl font-semibold ${styles.myFont2}`}>Our Founding Story</h1>
+                </div>
+                <h1 className={`text-[25px] text-[#343434] font-[600] grotesk  my-8 xs:text-[20px] ${styles.myFont2}`}>Paper Crown was founded by gender and development specialist Katie Carlson, a Canadian feminist activist and resident of Rwanda since 2012.</h1>
+                <h1 className='text-[20px] text-black font-[300] roboto my-8 xs:text-[18px]'>Throughout her time spent living and working in the country, Katie was continually inspired by the stories of Rwanda’s girls and young women, who persistently struggled with issues of discrimination and violence despite the strong leadership and political will shown across the country’s government to create a more gender equal society in Rwanda. After piloting a series of participatory workshops with adolescent girls that directly called out the gender issues permeating their daily lives and empowered girls to know their rights and believe in themselves, Katie realized there was tremendous potential to break the intergenerational cycle of gender inequality and violence by spending meaningful time with young people and changing hearts and minds for the better - through feminist critical thinking and the development of strong personal self-confidence and leadership skills that would lead to community advocacy and transformative gender norms change.</h1>
+                <button className='h-[60px] text-white text-lg mt-3 px-8 self-start bg-[#D71A30] rounded-xl flex items-center'>
+                    Visit Paper Crown
+                    <IoIosArrowRoundForward className='text-3xl ml-1'/>
+                </button>
+                <div className='relative w-full'>
+                </div>
             </div>
         </div>
         <Footer/>
