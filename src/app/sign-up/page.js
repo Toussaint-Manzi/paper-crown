2a461bb41f9f071d637ce62c5698aa7a8e45bcb5
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2024-07-16 22:39:24 +0200

    added section of signup

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -1,7 +1,10 @@
 "use client";
-import React from 'react'
+import React from 'react';
 import Stepper from 'react-stepper-horizontal';
 import styles from '../styles/home.module.css';
+import logo from '../../../public/assets/images/logo.png';
+import Image from 'next/image';
+
 const Signup = () => {
   return (
     <div className='bg-white w-full h-screen flex justify-between'>
@@ -40,8 +43,13 @@ const Signup = () => {
             </form>
             <h1 className='w-[50%] text-[18px] text-center text-black mt-3' >Already have account? <span className='text-[#D71A30]'>Login</span></h1>
         </div>
-        <div className='w-1/2 h-screen bg-[#fcf2f6]'>
-
+        <div className='w-1/2 bg-[#fcf2f6] py-20 px-20'>
+            <div className='py-[25%] w-[90%] flex flex-col justify-center items-center'>
+                <div className='w-40 h-20 relative mb-5'>
+                    <Image src={logo} fill alt="Logo" />
+                </div>
+                <h1 className={`text-black opacity-100 z-50 font-semibold text-[32px] w-[100%] mx-auto my-4`}>Join us in fostering equality and empowerment for all by becoming a part of our vibrant community dedicated to sharing knowledge and practices on gender transformative approaches and feminist practice</h1>
+            </div>
         </div>
     </div>
   )
