8226d478e6f4d28aa0aa9465d3aec408f735096e
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2024-07-25 11:21:58 +0200

    added reduxpersist

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,12 +1,32 @@
-import { configureStore, Tuple } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import signupReducer from './features/signup/signupStepSlice';
 import authReducer from './features/auth/authSlice';
 
+const rootReducer = combineReducers({
+  signup: signupReducer,
+  authData: authReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const makeStore = () => {
   return configureStore({
-    reducer: {
-      signup: signupReducer,
-      authData: authReducer
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+        },
+      }),
   });
 };
+
+export const store = makeStore();
+export const persistor = persistStore(store);
